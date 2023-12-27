64c398ca3d7e77894e2190acdbe8741fadba6a2a
Alex Yong <alexjyong@gmail.com>
2023-12-27 23:39:13 +0000

    updates

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 # Question of the Day Bot
 
-Question of the Day Bot! Inspired by https://github.com/KevinNovak/QOTD-Bot-Docs. 
+Question of the Day Bot! Inspired by https://github.com/KevinNovak/QOTD-Bot-Docs and https://github.com/mrrfv/discord-webhook-qotd-bot
 
-To run on your discord server, set up a bot with [guide](https://discordpy.readthedocs.io/en/stable/discord.html).
-
+Set up a webhook on your discord server with [this guide.](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks)
 Clone this repo onto the server that you plan to run the bot out of. Within the repo, create a `.env` file.
 
 With your favorite editor, add this to the file:
 ```
-TOKEN='tokenFromTheLastStepHere'
-BOTAUTHOR='WhatEverYouWantTheNameToBe'
+WEBHOOK='YourWebHookURLHere'
 ```
 
-(If you don't add in BOTAUTHOR, it will use the name `SysTD` by default.
+Run `pip install python-dotenv`. Depending on your system, you may or may not need to `pip install` other modules as well.
 
+Then run 
+`python3 qotd.py`
 
-install discord.py and python-dotenv via pip, then run
-`python3 systd.py`
-
-Or run via Docker with
-`docker build -t discord-bot .`
-then
-`docker run -d -t discord-bot` 
+This will run the job, read a question from questionsList.json, and add it to usedQuestions.json.  When the number of questions is equal to the used questions, the usedQuestions.json file is nuked, and the bot starts again.
--- qotd.py
+++ qotd.py
@@ -1,54 +1,62 @@
-import requests
+import os
 import json
 import random
-from datetime import datetime  # Import the datetime module
+from datetime import datetime
+from dotenv import load_dotenv
+import requests
 
-webhook_url = 'REDACTED' #make this an argument later
+def load_json_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from {filename}.")
+        exit(1)
 
-# Try to load questions from the JSON file
-try:
-    with open('questionsList.json', 'r') as json_file:
-        data = json.load(json_file)
-except FileNotFoundError:
-    print("The questionsList.json file was not found.")
+def save_json_file(filename, data):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+# Load environment variables
+load_dotenv()
+webhook_url = os.getenv('WEBHOOK_URL')
+if not webhook_url:
+    print("Webhook URL not found.")
     exit(1)
-except json.JSONDecodeError:
-    print("Error decoding JSON from the file.")
-    exit(1)
 
-# Extract the list of questions
-questions = data.get('questions', [])
+# Load questions and used questions
+questions = load_json_file('questionsList.json').get('questions', [])
+used_questions = load_json_file('usedQuestions.json')
 
-# Check if there are questions in the list
-if not questions:
-    print("No questions found in the JSON file.")
-    exit(1)
+# If all questions have been used, reset the used questions
+if len(used_questions) >= len(questions):
+    used_questions = []
 
-# Choose a random question
-random_question = random.choice(questions)
+# Choose a question not in used questions
+random_question = random.choice([q for q in questions if q not in used_questions])
+used_questions.append(random_question)
+save_json_file('usedQuestions.json', used_questions)
 
 # Create an embed for the message
 embed = {
     "title": "Question of the Day",
     "description": random_question,
-    "color": 0x00ff00,  # Green color
-    "timestamp": str(datetime.utcnow()),  # Corrected UTC timestamp
+    "color": 0x00ff00,
+    "timestamp": str(datetime.utcnow()),
     "footer": {
         "text": "Discord QOTD Bot"
     }
 }
 
-# Create the payload with the embed
-payload = {
-    "embeds": [embed]
-}
+# Payload with the embed
+payload = {"embeds": [embed]}
 
-# Send the POST request to the webhook URL with error handling
+# Send the POST request to the webhook URL
 try:
     response = requests.post(webhook_url, json=payload)
-    response.raise_for_status()  # Raises HTTPError for bad requests
+    response.raise_for_status()
     print("Random question successfully posted to the webhook.")
-except requests.exceptions.HTTPError as http_err:
-    print(f"HTTP error occurred: {http_err}")
 except requests.exceptions.RequestException as err:
     print(f"Error occurred: {err}")
